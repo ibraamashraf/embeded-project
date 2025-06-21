3da5db01683fa1a36ab0b77e09f3267f8d901a0f
ibraamashraf <ibraam.yacoup03@eng-st.cu.edu.eg>
2025-06-21 06:48:03 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+# main.tf - Local backend version
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
+  }
+}
+
+resource "null_resource" "example" {
+  triggers = {
+    timestamp = timestamp()
+  }
+}
